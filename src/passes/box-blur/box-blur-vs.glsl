d2efa93998d8171f7c90c28cd7c2a7e5d3139b6a
Fabien Motte <contact@fabienmotte.com>
2016-05-21 16:37:39 +0200

    Fix unread varying 'vUv' in fragment shader on box-blur pass

--- src/passes/box-blur/box-blur-vs.glsl
+++ src/passes/box-blur/box-blur-vs.glsl
@@ -1,27 +1,23 @@
-// non-dependent texture read according to : 
+// non-dependent texture read according to :
 // http://xissburg.com/faster-gaussian-blur-in-glsl/
 
-varying vec2 vUv;
 varying vec2 blurTexCoords[9];
 uniform vec2 delta;
 uniform vec2 resolution;
 
 void main() {
 
-  vUv = uv;
   vec2 inc = delta / resolution;
 
-
-  blurTexCoords[ 0 ] = vUv - inc * 4.;
-  blurTexCoords[ 1 ] = vUv - inc * 3.;
-  blurTexCoords[ 2 ] = vUv - inc * 2.;
-  blurTexCoords[ 3 ] = vUv - inc * 1.;
-  blurTexCoords[ 4 ] = vUv;
-  blurTexCoords[ 5 ] = vUv + inc * 1.;
-  blurTexCoords[ 6 ] = vUv + inc * 2.;
-  blurTexCoords[ 7 ] = vUv + inc * 3.;
-  blurTexCoords[ 8 ] = vUv + inc * 4.;
-  
+  blurTexCoords[ 0 ] = uv - inc * 4.;
+  blurTexCoords[ 1 ] = uv - inc * 3.;
+  blurTexCoords[ 2 ] = uv - inc * 2.;
+  blurTexCoords[ 3 ] = uv - inc * 1.;
+  blurTexCoords[ 4 ] = uv;
+  blurTexCoords[ 5 ] = uv + inc * 1.;
+  blurTexCoords[ 6 ] = uv + inc * 2.;
+  blurTexCoords[ 7 ] = uv + inc * 3.;
+  blurTexCoords[ 8 ] = uv + inc * 4.;
 
   gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 
