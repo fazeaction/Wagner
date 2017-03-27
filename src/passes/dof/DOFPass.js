3cc98b3e87348bcc6a563c39dd7ea7570c2f9d56
Guillaume Gouessan <guillaume.gouessan@gmail.com>
2017-03-27 12:34:11 +0200

    Remove useless uniform

--- src/passes/dof/DOFPass.js
+++ src/passes/dof/DOFPass.js
@@ -15,7 +15,6 @@ function DOFPass(options) {
   this.params.focalDistance = options.focalDistance || 0.01;
   this.params.aperture = options.aperture || .005;
   this.params.tBias = options.tBias || null;
-  this.params.blurAmount = options.blurAmount || 1;
 
 }
 
@@ -28,7 +27,6 @@ DOFPass.prototype.run = function(composer) {
   this.shader.uniforms.tBias.value = this.params.tBias;
   this.shader.uniforms.focalDistance.value = this.params.focalDistance;
   this.shader.uniforms.aperture.value = this.params.aperture;
-  this.shader.uniforms.blurAmount.value = this.params.blurAmount;
 
   this.shader.uniforms.delta.value.set( 1, 0 );
   composer.pass(this.shader);
